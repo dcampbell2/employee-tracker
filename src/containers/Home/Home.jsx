7289124c3380a964d4f90258bf9634ce0d9c4296
Dame <dcampbell7095@gmail.com>
2021-01-22 18:29:33 -0500

    adding base table to use for employees

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,34 +1,56 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import API from "../../utils/API/API";
 
 class Home extends Component {
-    state = {
-        name: "",
-        image: "",
-        email: ""
-    }
-    render() {
-        return (
-            <div className="container">
-            <div className="row">
-              <div className="col-12">
-                <ul className="nav nav-pills nav-fill">
-                  <li className="nav-item">
-                      Name
-                  </li>
-                  <li className="nav-item">
-                      Phone
-                  </li>
-                  <li className="nav-item">
-                  </li>
-                  <li className="nav-item">
-                      Email
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        );
-    }
+  state = {
+    name: "",
+    image: "",
+    email: "",
+  };
+
+  // componentDidMount(){
+  //     API.getUsers().then((res) => {
+  //         console.log(res)
+  //     }).catch((err) => {
+  //         console.log(err)
+  //     })
+  // }
+  render() {
+    return (
+      <div className="container">
+        <table class="table table-dark">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Employee</th>
+              <th scope="col">Phone</th>
+              <th scope="col">Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Larry</td>
+              <td>the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default Home;
